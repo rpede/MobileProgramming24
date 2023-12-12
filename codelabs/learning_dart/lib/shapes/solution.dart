a880191565d6a68571644009e8a07f034918aadd
Rasmus Pedersen <18396856+rpede@users.noreply.github.com>
2023-12-12 10:31:16 +0100

    Demo dart using simple shapes as example

---
+++ codelabs/learning_dart/lib/shapes/solution.dart
@@ -0,0 +1,59 @@
+import 'dart:math' as math;
+
+class Point {
+  final num x;
+  final num y;
+  const Point(num x, num y)
+      : this.x = x,
+        this.y = y;
+
+  @override
+  String toString() {
+    return "$runtimeType($x, $y)";
+  }
+}
+
+const Point origin = Point(0, 0);
+
+class Line {
+  Point a;
+  Point b;
+  Line({required a, required b})
+      : this.a = a,
+        this.b = b;
+
+  calculateLength() {
+    return math.sqrt(math.pow(b.x - a.x, 2) + math.pow(b.y - a.y, 2));
+  }
+}
+
+extension PointExtensions on Point {
+  Line to(Point other) {
+    return Line(a: this, b: other);
+  }
+}
+
+abstract class Shape {
+  Point center;
+  Shape({required this.center});
+  bool includes(Point point);
+}
+
+class Circle extends Shape {
+  num radius;
+  Circle({required super.center, required this.radius});
+
+  // A = πr2
+  get area => math.pow(radius, 2) * math.pi;
+
+  @override
+  bool includes(Point point) {
+    final distance = center.to(point).calculateLength();
+    return distance < radius;
+  }
+
+  @override
+  String toString() {
+    return "$runtimeType(center: $center, radius: $radius)";
+  }
+}
